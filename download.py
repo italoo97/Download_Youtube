b3ccfb9ce093564909c2b4987345932e73ce3cb3
italoo97 <italohugodf39@gmail.com>
2025-01-21 01:53:54 -0300

    Create download.py

---
+++ download.py
@@ -0,0 +1,20 @@
+import yt_dlp
+
+LINK="https://www.youtube.com/watch?v={ID_video}"
+
+def download_youtube_video(url, output_path="."):
+    try:
+        ydl_opts = {
+            'format': 'best',  # best quality
+            'outtmpl': f'{output_path}/%(title)s.%(ext)s',  # Name saved
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print("Download concluído!")
+    except Exception as e:
+        print(f"Erro ao baixar o vídeo: {e}")
+
+video_url = LINK
+download_path = r"C:\downloads"  # Directori destination
+
+download_youtube_video(video_url, download_path)
